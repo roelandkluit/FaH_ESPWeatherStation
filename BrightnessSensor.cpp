520cfbe59e8bb6347d890dea6123fd9d469145e0
Roeland Kluit <roeland@kluit.net>
2023-10-31 08:57:10 +0100

    Added smoothing brightness messurements

--- BrightnessSensor.cpp
+++ BrightnessSensor.cpp
@@ -6,26 +6,52 @@
 
 void BrightnessSensor::Process()
 {
-    if (millis() - previousWeatherInfoCollectMillis >= (BIGHTNESS_REFRESH_INTERVAL))
+    if (millis() - previousWeatherInfoCollectMillis >= (BIGHTNESS_UPDATE_INTERVAL))
     {
         previousWeatherInfoCollectMillis = millis();
-        uint16_t tBrightnessLightLevel = analogRead(PIN_Sensor);
-
-        if (forceUpdateCounter >= 10 || tBrightnessLightLevel != this->BrightnessLightLevel)
+        if (ProcessReading(analogRead(PIN_Sensor)))
         {
-            this->BrightnessLightLevel = tBrightnessLightLevel;
-            if (__CB_BRIGHTNESS_CHANGED != NULL)
+            //Only update once after full loop of all array values
+            uint16_t tBrightnessLightLevel = averageBrightnessLightLevel;
+            if (forceUpdateCounter >= 10 || averageBrightnessLightLevel != this->BrightnessLightLevel)
             {
-                uint16_t B2 = tBrightnessLightLevel + pow((tBrightnessLightLevel / 15), 2);
-                __CB_BRIGHTNESS_CHANGED(B2);
-                forceUpdateCounter = 0;
+                this->BrightnessLightLevel = averageBrightnessLightLevel;
+                if (__CB_BRIGHTNESS_CHANGED != NULL)
+                {
+                    uint16_t B2 = tBrightnessLightLevel + pow((tBrightnessLightLevel / 15), 2);
+                    __CB_BRIGHTNESS_CHANGED(B2);
+                    forceUpdateCounter = 0;
+                }
+            }
+            else
+            {
+                forceUpdateCounter++;
             }
         }
-        else
-        {
-            forceUpdateCounter++;
-        }
     }
+}
+
+bool BrightnessSensor::ProcessReading(const unsigned int& Value)
+{
+    // subtract the last reading:
+    total = total - readings[readIndex];
+    // read from the sensor:
+    readings[readIndex] = Value;
+    // add the reading to the total:
+    total = total + readings[readIndex];
+    // advance to the next position in the array:
+    readIndex = readIndex + 1;
+
+    // if we're at the end of the array...
+    if (readIndex >= NUMBER_OF_PROBES)
+    {
+        // ...wrap around to the beginning:
+        readIndex = 0;
+        // calculate the average:
+        averageBrightnessLightLevel = total / NUMBER_OF_PROBES;
+        return true;
+    }
+    return false;
 }
 
 BrightnessSensor::BrightnessSensor(const uint8_t& pin)
--- BrightnessSensor.h
+++ BrightnessSensor.h
@@ -10,10 +10,13 @@
 #endif
 
 #define BIGHTNESS_REFRESH_INTERVAL 30002 // Once every 30 seconds
+#define NUMBER_OF_PROBES 10 //Number of probes for average value callculation
+#define BIGHTNESS_UPDATE_INTERVAL BIGHTNESS_REFRESH_INTERVAL / NUMBER_OF_PROBES
+
 
 class BrightnessSensor {
 public:
-	void Process();
+	void Process();	
 	BrightnessSensor(const uint8_t& pin);
 	void SetOnLuxValueChangeEvent(void(*callback)(const uint16_t& Luxvalue)) { __CB_BRIGHTNESS_CHANGED = callback; }
 	uint16_t GetBrightness();
@@ -24,6 +27,11 @@ private:
 	uint8_t PIN_Sensor;
 	uint16_t BrightnessLightLevel = 0xFFFF;
 	uint8_t forceUpdateCounter = 0;
+	unsigned int readings[NUMBER_OF_PROBES] = { 0 };  // the readings from the analog input
+	unsigned int readIndex = 0;          // the index of the current reading
+	unsigned int total = 0;              // the running total
+	unsigned int averageBrightnessLightLevel = 0;            // the average
+	bool ProcessReading(const unsigned int& Value);
 };
 
 #endif
